ac1ef5e2ef86cdce2b23c8ba5685a2f2af20a45f
Calegariii <gabrielcalega@gmail.com>
2026-09-19 20:40:37 -0300

    MEDIA NOTAS

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Olá Mundo")
+print("=" * 30)
+print("Boas Vindas ao Sistemas de Gerenciamento de Notas")
+print("=" * 30)
+
+nomes = []
+media = []
+
+av1 = []
+av2 = []
+av3 = []
+
+def cadastrar_aluno():
+    while True:
+        print()
+
+
---
+++ medianotas.py
@@ -0,0 +1,8 @@
+# Definir a funcao para calcular a média de duas notas
+def calcular_media(nota1, nota2):
+    media = (nota1 + nota2) / 2
+    return media
+
+# Utilizar a função 
+resultado = calcular_media(8.5, 7.5)
+print(f"A média é: {resultado}")  # Saída: A média é: 8.0
